9c8848662a3ff9bb63d996fb62ae35298c094fea
Fred Noraz <fred.noraz@free.fr>
2018-01-07 22:05:38 +0100

    Add log prints for debug

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,7 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //Array of words
+        String [] words = {"One", "Two", "Three", "Four", "Five",
+                "Six", "Seven", "Eight", "Nine", "Ten"};
+
+        //Log tag example
+        for(int i=0; i<10; i++) {
+            Log.v("NumbersActivity", "word at index[" + i + "]: " + words[i]);
+        }
     }
 }
