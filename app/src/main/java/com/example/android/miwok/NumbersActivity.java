39a7c702afcf54aa52b9d9ea6b088db414214b45
Fred Noraz <fred.noraz@free.fr>
2018-01-08 21:12:04 +0100

    Add log prints for debug with ArrayList

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
@@ -11,13 +13,22 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        //Array of words
-        String [] words = {"One", "Two", "Three", "Four", "Five",
-                "Six", "Seven", "Eight", "Nine", "Ten"};
+        //ArrayList of words
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("One");
+        words.add("Two");
+        words.add("Three");
+        words.add("Four");
+        words.add("Five");
+        words.add("Six");
+        words.add("Seven");
+        words.add("Eight");
+        words.add("Nine");
+        words.add("Ten");
 
         //Log tag example
         for(int i=0; i<10; i++) {
-            Log.v("NumbersActivity", "word at index[" + i + "]: " + words[i]);
+            Log.v("NumbersActivity", "word at index[" + i + "]: " + words.get(i));
         }
     }
 }
