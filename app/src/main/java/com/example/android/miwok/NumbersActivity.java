61814c5b59be4cc386fcbd144e6bbb6cdc35fbf0
Fred Noraz <fred.noraz@free.fr>
2018-01-09 22:10:43 +0100

    Add usage of ListView + Adapter

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -28,14 +30,9 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("Nine");
         words.add("Ten");
 
-        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
-
-        int count = 0;
-        while (count <words.size()) {
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(count));
-            rootView.addView(wordView);
-            count += 1;
-        }
+        //https://developer.android.com/guide/topics/ui/declaring-layout.html#AdapterViews
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
     }
 }
